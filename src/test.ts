08a2ccbf9b167925d8c201b14e839d2b33cb9b8a
codersomu <arvind.somu@gmail.com>
2024-08-26 07:01:29 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,8 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+//https://www.google.com/search?&q=nse%3A+CDSL
+// https://screener.in/company/CDSL/
+// https://www.nseindia.com/get-quotes/equity?symbol=CDSL
+//  https://in.tradingview.com/chart/gs6glM5Y/?symbol=NSE%3ACDSL
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
